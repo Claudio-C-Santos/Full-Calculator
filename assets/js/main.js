dc1d836c13623bbeab6b392dde1c9933bbc91066
Claudio-C-Santos <cula.santos@gmail.com>
2021-01-28 11:27:32 +0000

    calculator built, just need to display result

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,106 @@
 var calculator = {
-    displayValue: 0,
+    displayValue: '0',
     firstOperand: null,
     waitingForSecondOperand: false,
     operator: null,
 }
 
+function inputDigit(digit) {
+    let { displayValue,waitingForSecondOperand } = calculator;
+    
+    if (waitingForSecondOperand === true || calculator.displayValue == '0') {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else  {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+    console.log(calculator)
+}
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+        return parseFloat(firstOperand) + parseFloat(secondOperand);
+    } else if (operator === '-') {
+        return parseFloat(firstOperand) - parseFloat(secondOperand);
+    } else if (operator === '/') {
+        return parseFloat(firstOperand) / parseFloat(secondOperand);
+    } else if (operator === '*') {
+        return parseFloat(firstOperand) * parseFloat(secondOperand);
+    }
+
+    return secondOperand;
+}
+
+function handleOperator(nextOperator) {
+    let {firstOperand, displayValue, operator} = calculator;
+    let inputValue = parseFloat(displayValue);
+    let result;
+
+    if (firstOperand === null && !isNaN(inputValue)) {
+        calculator.firstOperand=inputValue;
+    } else if (operator) {
+        result=calculate(firstOperand,displayValue,operator);
+
+    }
+  
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+
+    console.log(calculator)
+}
+
+
+
 function updatedDisplay() {
     let display = document.querySelector('#innerDisplay');
-    display.textContent = calculator.displayValue;
-    return calculator.displayValue;
+    display.value = calculator.displayValue;
 }
 
 updatedDisplay()
-console.log(updatedDisplay())
+
+let keys = document.querySelector('#keysArea');
+keys.addEventListener('click', function(event) {
+    let {target} = event;
+
+    if (!target.matches("button")) {
+        return;
+    } 
+
+    if (target.matches('.number')) {
+        inputDigit(target.value);
+        updatedDisplay();
+        return;
+    }
+
+    if (target.matches('.operator')) {
+        handleOperator(target.value);
+        return;
+    }
+
+    if (target.matches('.decimal')) {
+        inputDecimal(target.value);
+        updatedDisplay();
+        return;
+    }
+
+    if (target.matches('.other')) {
+        console.log(target.value);
+        return;
+    } 
+    if (target.matches('.calculate')) {
+        handleOperator(target.value);
+        updatedDisplay();
+        console.log(target.value);
+        return;
+    } 
+});
 
 
 
